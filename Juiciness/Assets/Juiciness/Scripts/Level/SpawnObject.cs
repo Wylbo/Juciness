1a1bb1f545aeeaeac8f6fa37101f1c5c7766c592
Maximilien Galea <m.galea@student.isartdigital.com>
2019-12-09 19:53:52 +0100

    start procedural generation

---
+++ Juiciness/Assets/Juiciness/Scripts/Level/SpawnObject.cs
@@ -0,0 +1,19 @@
+///-----------------------------------------------------------------
+/// Author : Maximilien Galea
+/// Date : 09/12/2019 19:45
+///-----------------------------------------------------------------
+
+using UnityEngine;
+
+namespace Com.MaximilienGalea.Juiciness.Juiciness.Level {
+	public class SpawnObject : MonoBehaviour {
+
+        [SerializeField] GameObject[] objects;
+	
+		private void Start () {
+            int rand = Random.Range(0, objects.Length);
+            Instantiate(objects[rand], transform.position, Quaternion.identity);
+		}
+		
+	}
+}
