033cce0aa8ac4a9ac7a96476f08b55b2bdc1ba42
Maximilien Galea <m.galea@student.isartdigital.com>
2019-12-10 02:49:24 +0100

    create level generator

---
+++ Juiciness/Assets/Juiciness/Scripts/Dungeon/LevelGenerator.cs
@@ -0,0 +1,24 @@
+///-----------------------------------------------------------------
+/// Author : Maximilien Galea
+/// Date : 10/12/2019 02:42
+///-----------------------------------------------------------------
+
+using UnityEngine;
+
+namespace Com.MaximilienGalea.Juiciness.Juiciness.Dungeon {
+	public class LevelGenerator : MonoBehaviour {
+
+        [SerializeField] private GameObject[] leftRooms;
+        [SerializeField] private GameObject[] rightRooms;
+        [SerializeField] private GameObject[] topRooms;
+        [SerializeField] private GameObject[] bottomRooms;
+
+        private void Start () {
+			
+		}
+		
+		private void Update () {
+			
+		}
+	}
+}
